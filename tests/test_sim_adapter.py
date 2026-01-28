KevinRGeurts/tkAppFramework

commit 39d8e3be4e42dd0f4cfe3619f7f48ae735a3b3fd

(1) SimulatorAdapter now expects run() and load_and_run() methods to be extended rather than implemented, and for super().run() or super().load_and_run() to be called at the end of the subclasses extended method. The SimulatorAdapter versions of these methods logger.info('<<SimulatorReportsCompletion>>') to the simulator's logger. (2) tkSimulatorApp.onEndSimulator() behavior refactored. GUI cleanup now handled by new cleanup_after_simulator_ends() method, which is called by tkSimulatorViewManager.handle_model_update() method, when the retrieved LogRecord message is '<<SimulatorReportsCompletion>>' (3) DemoSimulatorAdapter in main.py modified to accomodate new SimulatorAdapter subclass expectations.

src/tkAppFramework/main.py
@@ -242,6 +242,7 @@ class DemoSimulatorAdapter(SimulatorAdapter):
         :return: None
         """
         self.simulator.go()
+        super().run()
         return None
 
     def load_and_run(self):
@@ -250,8 +251,9 @@ class DemoSimulatorAdapter(SimulatorAdapter):
         :return: None
         """
         logger = logging.getLogger('demo_simulator_logger')
-        logger.info(f"Loading functinality not implemented for DemoSimulator, so just lauching a simulation...")
+        logger.info(f"Loading functionality not implemented for DemoSimulator, so just lauching a simulation...")
         self.simulator.go()
+        super().load_and_run()
         return None
     
 

src/tkAppFramework/sim_adapter.py
@@ -12,9 +12,9 @@ Simulator requirements:
     to gracefully terminate itself.
 
 Subclasses must:
-(1) Implement run() - Which should call a method of self._simulator to start a simulation
+(1) Extend run() - Which should call a method of self._simulator to start a simulation, then call super().run().
 Subclasses may also implement:
-(1) load_and_run() - Which should load a simulation state and run the simulation with that state.
+(1) load_and_run() - Which should load a simulation state and run the simulation with that state, and then call super().load_and_run()
 
 Exported Classes:
     SimulatorAdapter -- Interface (abstract base) class for classes that adapt a simulator to the
@@ -68,6 +68,7 @@ class SimulatorAdapter(object):
         :parameter logging_level: The threshold for the queue handler, e.g. logging.INFO or logging.DEBUG
         """
         self.simulator = sim
+        self._logger_name = logger_name
         self._setup_logging(logger_name, logging_queue, logging_level)
 
     @property
@@ -80,18 +81,25 @@ class SimulatorAdapter(object):
 
     def run(self):
         """
-        This abstract method must be implemented by subclasses. When called, it should start a simulation.
+        This method must be extended by subclasses. When called, it should start a simulation. super() version should be
+        called at end.
         :return: None
         """
-        raise NotImplementedError
+        # Get the adaptee simulator's logger
+        logger = logging.getLogger(self._logger_name)
+        # Log that simulator run ended, so that tkSimulatorApp knows 
+        logger.info('<<SimulatorReportsCompletion>>')
         return None
 
     def load_and_run(self):
         """
-        This abstract method must be implemented by subclasses. When called, it should load a simulation state and run the
-        simulation with that state.
+        This method must be extended by subclasses. When called, it should load a simulation state and run the
+        simulation with that state. super() version should be called at end.
         """
-        raise NotImplementedError
+        # Get the adaptee simulator's logger
+        logger = logging.getLogger(self._logger_name)
+        # Log that simulator run ended, so that tkSimulatorApp knows 
+        logger.info('<<SimulatorReportsCompletion>>')
         return None
     
     def _setup_logging(self, name='', queue=None, log_level = logging.INFO):

src/tkAppFramework/tkSimulatorApp.py
@@ -190,17 +190,27 @@ class tkSimulatorApp(tkApp):
         :return: None
         """
         if self._sim_thread:
-            self._query_view_manager.reset_widgets()
             self.request_simulator_end()
-            # Disable File | End Simulator menu item, since no simulator will now be running
-            self._menuConfigDict['File'][0].entryconfig(self._menuConfigDict['File'][1]['End Simulator'][1], state=tk.DISABLED)
-            # enable File | Start Simulator, since now we have no running simlator
-            self._menuConfigDict['File'][0].entryconfig(self._menuConfigDict['File'][1]['Start Simulator'][1], state=tk.NORMAL)
-            # enable File | Load Simulation, since now we have no running simlator
-            self._menuConfigDict['File'][0].entryconfig(self._menuConfigDict['File'][1]['Load Simulation'][1], state=tk.NORMAL)
         else:
             pass
 
+        return None
+
+    def cleanup_after_simulator_ends(self):
+        """
+        Utility function called to clean up GUI state when simulator ends.
+        :return: None
+        """
+        if self._sim_thread is not None:
+            assert(self._sim_thread.is_alive()==False,'Simulation thread unexpectedly found alive in tkSimulatorApp._cleanup_after_simulator_ends().')
+            self._sim_thread = None
+        self._query_view_manager.reset_widgets()
+        # Disable File | End Simulator menu item, since no simulator will now be running
+        self._menuConfigDict['File'][0].entryconfig(self._menuConfigDict['File'][1]['End Simulator'][1], state=tk.DISABLED)
+        # enable File | Start Simulator, since now we have no running simlator
+        self._menuConfigDict['File'][0].entryconfig(self._menuConfigDict['File'][1]['Start Simulator'][1], state=tk.NORMAL)
+        # enable File | Load Simulation, since now we have no running simlator
+        self._menuConfigDict['File'][0].entryconfig(self._menuConfigDict['File'][1]['Load Simulation'][1], state=tk.NORMAL)
         return None
 
     def request_simulator_end(self):
@@ -220,7 +230,6 @@ class tkSimulatorApp(tkApp):
         if self._sim_thread:
             end_sim_response = tkAppFramework.tkUserQueryReceiver.QueryResponse(query_response='<<QueryingThreadTerminationRequest>>', query_ID='')
             UserResponseCollector.UserQueryReceiver.UserQueryReceiver_GetCommandReceiver().put_response_in_queue(end_sim_response)
-            self._sim_thread = None
         else:
             pass
             

src/tkAppFramework/tkSimulatorViewManager.py
@@ -71,6 +71,12 @@ class tkSimulatorViewManager(tkViewManager):
         if info is not None:
             # Make sure we are retrieving what we think we are retrieving, that is, a LogRecord object
             assert(isinstance(info, LogRecord))
+            
+            # Check if the retrieved LogRecord is informing us that simulator reports completing.
+            if info.message == '<<SimulatorReportsCompletion>>':
+                # Ask app to clean up.
+                self.master.cleanup_after_simulator_ends()
+
             # Put the message from the Log Record in the SimulatorShowInfoWidget
             self._info_widget.insert_end(info.message)
         return None

commit ee05cbb2fbedbe28a0d1b054528ad248e971b4fd

Fixed unittests for SimulatorModel and SimulatorAdapter, which were broken by commit: 39d8e3be4e42dd0f4cfe3619f7f48ae735a3b3fd

tests/test_SimulatorModel.py
@@ -6,6 +6,7 @@ This module provides unit tests for the SimulatorModel class.
 # Standard
 import unittest
 import tkinter as tk
+import logging
 
 # Local
 from tkAppFramework.SimulatorModel import SimulatorModel
@@ -14,6 +15,22 @@ from tkAppFramework.dummy_AppViewMgr import TesttkApp
 
 
 class Test_SimulatorModel(unittest.TestCase):
+    def setUp(self):
+        # Create a logger called 'test_simulator_model_logger' This is NOT the root logger, which is one level up from here, and has no name.
+        logger = logging.getLogger('test_simulator_model_logger')
+        # This is the threshold level for the logger itself, before it will pass to any handlers, which can have their own threshold.
+        # Should be able to control here what the stream handler receives and thus what ends up going to stderr.
+        # Use this key for now:
+        #   DEBUG = debug messages sent to this logger will end up on stderr
+        #   INFO = info messages sent to this logger will end up on stderr
+        logger.setLevel(logging.INFO)
+        # Set up this highest level below root logger with a stream handler
+        sh = logging.StreamHandler()
+        # Set the threshold for the stream handler itself, which will come into play only after the logger threshold is met.
+        sh.setLevel(logging.INFO)
+        # Add the stream handler to the logger
+        logger.addHandler(sh) 
+
     def test_init_set_get_adapter(self):
         root = tk.Tk()
         app = TesttkApp(root)
@@ -31,14 +48,22 @@ class Test_SimulatorModel(unittest.TestCase):
         app = TesttkApp(root)
         adapt = SimulatorAdapter(logger_name='test_simulator_model_logger')
         mod = SimulatorModel(app, adapt)
-        self.assertRaises(NotImplementedError, mod.run)
+        # Test that expected info is logged
+        with self.assertLogs('test_simulator_model_logger', level=logging.INFO) as cm:
+            mod.run()
+        # Test that the info messages sent to the logger are as expected
+        self.assertEqual(cm.output[0], 'INFO:test_simulator_model_logger:<<SimulatorReportsCompletion>>')    
 
     def test_load_and_run(self):
         root = tk.Tk()
         app = TesttkApp(root)
         adapt = SimulatorAdapter(logger_name='test_simulator_model_logger')
         mod = SimulatorModel(app, adapt)
-        self.assertRaises(NotImplementedError, mod.load_and_run)
+        # Test that expected info is logged
+        with self.assertLogs('test_simulator_model_logger', level=logging.INFO) as cm:
+            mod.load_and_run()
+        # Test that the info messages sent to the logger are as expected
+        self.assertEqual(cm.output[0], 'INFO:test_simulator_model_logger:<<SimulatorReportsCompletion>>')
 
 
 if __name__ == '__main__':

tests/test_sim_adapter.py
@@ -5,12 +5,29 @@ This module provides unit tests for the SimulatorAdapter class.
 
 # Standard
 import unittest
+import logging
 
 # Local
 from tkAppFramework.sim_adapter import SimulatorAdapter
 
 
 class Test_SimulatorAdapter(unittest.TestCase):
+    def setUp(self):
+        # Create a logger called 'test_simulator_adapter_logger' This is NOT the root logger, which is one level up from here, and has no name.
+        logger = logging.getLogger('test_simulator_adapter_logger')
+        # This is the threshold level for the logger itself, before it will pass to any handlers, which can have their own threshold.
+        # Should be able to control here what the stream handler receives and thus what ends up going to stderr.
+        # Use this key for now:
+        #   DEBUG = debug messages sent to this logger will end up on stderr
+        #   INFO = info messages sent to this logger will end up on stderr
+        logger.setLevel(logging.INFO)
+        # Set up this highest level below root logger with a stream handler
+        sh = logging.StreamHandler()
+        # Set the threshold for the stream handler itself, which will come into play only after the logger threshold is met.
+        sh.setLevel(logging.INFO)
+        # Add the stream handler to the logger
+        logger.addHandler(sh)        
+    
     def test_init_set_get_simulator(self):
         # Here our dummy test simulator can be literaly anything. Here a float.
         test_sim = float(19.0)
@@ -24,11 +41,19 @@ class Test_SimulatorAdapter(unittest.TestCase):
 
     def test_run(self):
         adapt = SimulatorAdapter(logger_name='test_simulator_adapter_logger')
-        self.assertRaises(NotImplementedError, adapt.run)
+        # Test that expected info is logged
+        with self.assertLogs('test_simulator_adapter_logger', level=logging.INFO) as cm:
+            adapt.run()
+        # Test that the info messages sent to the logger are as expected
+        self.assertEqual(cm.output[0], 'INFO:test_simulator_adapter_logger:<<SimulatorReportsCompletion>>')    
 
     def test_load_and_run(self):
         adapt = SimulatorAdapter(logger_name='test_simulator_adapter_logger')
-        self.assertRaises(NotImplementedError, adapt.load_and_run)
+        # Test that expected info is logged
+        with self.assertLogs('test_simulator_adapter_logger', level=logging.INFO) as cm:
+            adapt.load_and_run()
+        # Test that the info messages sent to the logger are as expected
+        self.assertEqual(cm.output[0], 'INFO:test_simulator_adapter_logger:<<SimulatorReportsCompletion>>')    
 
 
 if __name__ == '__main__':